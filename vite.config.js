5fcbcacb8c9440039c8fb0aeb89f68f06580ac2f
Fawwaz <fawazadvan23@gmail.com>
2025-04-08 19:51:49 +0700

    fix: switch to HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Portfolio_Fawwazz/", // Harus SAMA persis dengan nama repo GitHub kamu
+  base: '/Portfolio_Fawwazz/',
   plugins: [react()],
-});
+})
