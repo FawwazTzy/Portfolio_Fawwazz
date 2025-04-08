ca53ff616f8a9ff509ec73ca319a72d410fb722b
Fawwaz <fawazadvan23@gmail.com>
2025-04-08 18:30:31 +0700

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Export konfigurasi yang benar
 export default defineConfig({
+  base: "/Portfolio_Fawwazz/", // Harus SAMA persis dengan nama repo GitHub kamu
   plugins: [react()],
-  server: {
-    port: 5173,  // pastikan port ini sesuai
-  }
 });
